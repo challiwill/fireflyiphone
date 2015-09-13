387981640ee3951849028a64bc998f964a7729cb
Corbin Halliwill <corbin.halliwill@berkeley.edu>
2015-09-13 10:46:16 -0700

    Initial Commit

---
+++ FireflyApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FireflyApp
+//
+//  Created by Black_Shark on 9/13/15.
+//  Copyright (c) 2015 Black_Shark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
